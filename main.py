092e9dfa3596be07f4d2094c08dbdbcadd1e23f5
Kevin <kevinxz01@gmail.com>
2022-05-02 13:35:14 -0500

    printed the version number from the cli

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cli_tree import __version__
 import cli_tree.cli as cli
 import argparse
 
@@ -15,10 +16,15 @@ Thanks for using my CLI Tree!
 def main():
     tree_cli = cli.Directories()
 
-    parser = argparse.ArgumentParser(description=PARSE_MESSAGE)
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description=PARSE_MESSAGE)
+    # args = parser.parse_args()
+
+    parse = argparse.ArgumentParser(description="CLI Tree for directory.")
+    parse.add_argument("-v", action="version", version=__version__,help="shows program's version number and exits")
+    args = parse.parse_args()
 
     print (args)
+
 
 if __name__ == "__main__":
     main()
